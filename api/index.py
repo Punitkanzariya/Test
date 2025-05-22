6d823e62606fae0998269c315c589fc7652992e4
Punit <Punit@TECH37-PC.Techsture.com>
2025-05-22 10:58:41 +0530

    Save

--- api/index.py
+++ api/index.py
@@ -1,7 +1,6 @@
-# api/index.py
 def handler(request):
     return {
         "statusCode": 200,
-        "headers": { "Content-Type": "application/json" },
-        "body": "Hello from Python on Vercel!"
+        "headers": {"Content-Type": "application/json"},
+        "body": '{"message": "Hello from a Vercel Python Serverless Function!"}'
     }
